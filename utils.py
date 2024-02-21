7e2f8bce80e836c16fb6931fb7890a9bcf754c3f
Anant Bhatia <anant.x.bhatia@gmail.com>
2024-02-20 18:30:09 -0600

    Utils

---
+++ utils.py
@@ -0,0 +1,21 @@
+import pandas as pd# Your list of items
+
+"""
+Input a list of Polygon Aggregates
+Outputs a DataFram with the json items parse into columns
+"""
+def polygon_to_df(items):
+    data = []
+    for item in items:
+        data.append({
+            "open": item.open,
+            "high": item.high,
+            "low": item.low,
+            "close": item.close,
+            "volume": item.volume,
+            "vwap": item.vwap,
+            "timestamp": item.timestamp,
+            "transactions": item.transactions,
+            "otc": item.otc
+        })# Creating a DataFrame
+    df = pd.DataFrame(data)
